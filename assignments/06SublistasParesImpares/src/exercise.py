7b8f19149bd5810714e968ab3546bd19d7eac50c
Francisco Vazquez <franvazgom@gmail.com>
2021-09-26 12:58:53 -0500

    V1

--- assignments/06SublistasParesImpares/src/exercise.py
+++ assignments/06SublistasParesImpares/src/exercise.py
@@ -1,38 +1,7 @@
 def main():
-    #escribe tu código abajo de esta línea
-
-    # CODIGO ############################
-
-    # Listando pares e impares -----------------------------
-    # Autor: ricardo.quintero@tec.mx
-
-    fin=False
-    lista=[]
-
-    while not fin:
-        numero=input()
-
-        if numero == "*":
-            break
-        lista.append(int(numero))
-
-    lp=[]
-    li=[]
-
-    for e in lista:
-        if int(e) % 2 == 0:
-            lp.append(int(e))
-        else:
-            li.append(int(e))
-
-    print("PARES")
-    print(lp)
-    print("IMPARES")
-    print(li)
-
-    # FIN CODIGO ########################
-
+    # escribe tu código abajo de esta línea
     pass
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
